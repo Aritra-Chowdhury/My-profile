3da8c152454e4d919c4da20b05fcabf6b11fc708
Aritra Chowdhury <aritra.chowdhury2@cognizant.com>
2019-04-24 00:15:27 +0530

    first commit

--- src/app/components/top/top.component.ts
+++ src/app/components/top/top.component.ts
@@ -11,7 +11,9 @@ export class TopComponent implements OnInit {
 
   ngOnInit() {
    this.typeBio();
+   this.typeBioMb();
   }
+
 
   typeBio()
   {
@@ -27,6 +29,23 @@ export class TopComponent implements OnInit {
  };
  
  const typed = new Typed('.typed', options);
+  }
+
+
+  typeBioMb()
+  {
+    const options = {
+      strings: ["To learn and function effectively in an organization and be able to deliver to the bottom-line.",
+      " Bringing strong computer skills, and personal attributes including devotion, meeting goals, inventiveness, and the attitude to follow through.",
+      " To constantly upgrade my knowledge and skills and make a difference in whatever I do.",
+              ],
+      typeSpeed: 100,
+      showCursor: true,
+      cursorChar: '|',
+      loop: true
+ };
+ 
+ const typedmb = new Typed('.typedmb', options);
   }
 
   
